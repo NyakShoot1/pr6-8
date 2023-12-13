8d395478c676dd7042936625903af604ad70892e
NyakShoot1 <danikaorlov@mail.ru>
2023-12-13 19:23:48 +0300

    ya

--- 1/app/services/task_service.py
+++ 1/app/services/task_service.py
@@ -19,15 +19,15 @@ class TaskService:
         return self.task_repo.get_tasks()
 
     def create_task(self, title: str, description: str, user_id: UUID):
-        response = requests.get(f"http://127.0.0.1:8001/api/user/{user_id}")
+        # response = requests.get(f"http://127.0.0.1:8001/api/user/{user_id}")
 
-        if response.status_code == 200:
-            new_task = Task(id=uuid.uuid4(), title=title, description=description, user_id=user_id,
+        # if response.status_code == 200:
+        new_task = Task(id=uuid.uuid4(), title=title, description=description, user_id=user_id,
                             status=TaskStatuses.ACTIVATE)
-            send_notification(new_task)
-            return self.task_repo.create_task(new_task)
-        else:
-            raise KeyError
+        send_notification(new_task)
+        return self.task_repo.create_task(new_task)
+        # else:
+        #     raise KeyError
 
     def done_task(self, task_id: UUID) -> Task:
         task = self.task_repo.get_task_by_id(task_id)
