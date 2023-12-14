86d1023a54d14c7f5431510c02d9e51a06f44877
NyakShoot1 <danikaorlov@mail.ru>
2023-12-14 12:42:36 +0300

    tests

--- 1/app/settings.py
+++ 1/app/settings.py
@@ -2,8 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    amqp_url: str = ""
-    postgres_url: str = "postgresql://secUREusER:StrongEnoughPassword)@51.250.26.59:5432/orlov"
+    amqp_url: str
+    postgres_url: str
+    port: str
     model_config = SettingsConfigDict(env_file='.env')
 
 
